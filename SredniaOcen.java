b8241bd8f80f24e1b393bab3a78d0d3274edfa30
JaSalJakub <162017719+JaSalJakub@users.noreply.github.com>
2024-04-06 12:50:12 +0200

    Add files via upload

--- SredniaOcen.java
+++ SredniaOcen.java
@@ -2,42 +2,28 @@ import java.util.Scanner;
 
 public class SredniaOcen {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);        
         System.out.println("Podaj oceny z poszczególnych przedmiotów (oddzielone spacją):");
-        String input = scanner.nextLine();
-        
-        // Dzielimy wprowadzone oceny na osobne ciągi znaków
-        String[] ocenyArray = input.split(" ");
-        
-        // Inicjalizujemy zmienne do przechowywania sumy ocen oraz liczby ocen
+        String input = scanner.nextLine();  
+        String[] ocenyArray = input.split(" ");        
         double sumaOcen = 0;
-        int liczbaOcen = 0;
-        
-        // Iterujemy przez każdy element tablicy ocen
+        int liczbaOcen = 0;        
         for (String ocenaStr : ocenyArray) {
-            try {
-                // Konwertujemy ocenę na liczbę zmiennoprzecinkową
+            try {               
                 double ocena = Double.parseDouble(ocenaStr);
                 
-                // Dodajemy ocenę do sumy
+                
                 sumaOcen += ocena;
                 
-                // Zwiększamy liczbę ocen
+                
                 liczbaOcen++;
             } catch (NumberFormatException e) {
-                // Jeżeli wprowadzona wartość nie jest liczbą, ignorujemy ją
+                
                 System.out.println("Błąd: " + ocenaStr + " nie jest liczbą. Ignoruję tę wartość.");
             }
         }
-        
-        // Obliczamy średnią arytmetyczną
-        double srednia = sumaOcen / liczbaOcen;
-        
-        // Wyświetlamy wynik
-        System.out.println("Średnia ocen: " + srednia);
-        
-        // Zamykamy scanner
+        double srednia = sumaOcen / liczbaOcen;       
+        System.out.println("Średnia ocen: " + srednia);        
         scanner.close();
     }
 }
